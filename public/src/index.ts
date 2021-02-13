fa5a02d6014bb9671c0a736cf6caf465ec1ace34
Jaroslav Procházka <jaroslav.prossv@gmail.com>
2021-02-13 13:01:29 +0100

    new button

---
+++ public/src/index.ts
@@ -0,0 +1,9 @@
+let data = {
+    "id": 1,
+    "first": "Jaroslav",
+    "last": "Prochazka",
+    "email": "xxx@xxx.com",
+    "phone": "000 111 222",
+    "location": "vrsovice, praha",
+    "project": "CRUDapp"
+}
